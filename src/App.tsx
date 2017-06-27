32b54f66a57bd35097a8dff519f191615c4d6fc3
Adhy <adhywiranatap@gmail.com>
2017-06-28 00:43:10 +0700

    add anime card tags

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,22 @@ class App extends React.Component<{}, {}> {
         <section className="app-wrapper">
           <h1>Animes for You</h1>
           <section className="anime-list">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(anime => (
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((anime, index) => (
               <div className="anime-card">
                 <div className="badge">TRENDING</div>
                 <div style={{ flex: 1 }}>
                   <img src="https://myanimelist.cdn-dena.com//images//anime//12//85221.jpg" width="100%" />
                 </div>
-                <div style={{ flex: 1, padding: 10, paddingTop: 0 }}>
+                <div style={{ flex: 1, paddingBottom: 10 }}>
                   <h2>Boku no Hero Academia Season 2</h2>
                   <span className="score">9.05</span>
+                  <div>
+                    <div className="tag">Shounen</div>
+                    <div className="tag">Action</div>
+                    <div className="tag">Seinen</div>
+                    { index % 2 === 0 && <div className="tag">Drama</div> }
+                    { index % 2 === 0 && <div className="tag">Superhero</div> }
+                  </div>
                 </div>
               </div>
             ))}
