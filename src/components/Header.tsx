5a5cf0ad8d0c884664ddd74bfaabbfaa95738330
Adhy <adhywiranatap@gmail.com>
2017-06-27 23:40:43 +0700

    update header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
+const menuButton = require('./menu.png');
+const searchButton = require('./search.png');
+
 export default () => (
   <div className="app-header">
+    <img src={menuButton} width="25px" height="25px" />
     <h1 className="app-logo">
       <span className="first">Couch</span>
       <span className="second">Otaku</span>
     </h1>
+    <img src={searchButton} width="25px" height="25px" />
   </div>
 );
