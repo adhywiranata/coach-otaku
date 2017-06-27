6dc7e465608baa98f0ad50d54d8dadd8108281ec
Adhy <adhywiranatap@gmail.com>
2017-06-27 22:48:32 +0700

    refine Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 
 export default () => (
   <div className="app-header">
-    <h2>Couch Otaku</h2>
+    <h1 className="app-logo">
+      <span className="first">Couch</span>
+      <span className="second">Otaku</span>
+    </h1>
   </div>
 );
