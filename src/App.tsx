9cf9aa163a39cc2b3d6c40ab925e3d5fa2626791
Adhy <adhywiranatap@gmail.com>
2017-06-28 00:06:57 +0700

    add anime list and cards

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,21 @@ class App extends React.Component<{}, {}> {
     return (
       <div className="app">
         <Header />
-        <p className="app-intro">
-          Super Complete Anime List Directory
-        </p>
+        <section className="app-intro">
+          <h1>Complete Anime List</h1>
+          <section className="anime-list">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(anime => (
+              <div className="anime-card">
+                <div style={{ flex: 1 }}>
+                  <img src="https://myanimelist.cdn-dena.com//images//anime//12//85221.jpg" width="100%" />
+                </div>
+                <div style={{ flex: 1 }}>
+                  <h2>Boku no Hero Academia Season 2</h2>
+                </div>
+              </div>
+            ))}
+          </section>
+        </section>
       </div>
     );
   }
