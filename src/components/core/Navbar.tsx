86fea27e527cfd0bfb49d6e8f5339448852ffd15
Adhy <adhywiranatap@gmail.com>
2017-06-28 14:14:01 +0700

    update navbar styles

--- src/components/core/Navbar.tsx
+++ src/components/core/Navbar.tsx
@@ -4,8 +4,14 @@ import './navbar.css';
 
 const Navbar = ({ isNavbarActive, toggleNavbar }: { isNavbarActive: boolean; toggleNavbar: any; }) => (
   <div className="navbar" style={{ left: isNavbarActive ? 0 : '-81vw' }}>
-    wowowoowow
-    <button onClick={toggleNavbar}>X</button>
+    <button onClick={toggleNavbar} className="btn-close">
+      <img src={require('./close.png')} width="30px" />
+    </button>
+    <header className="navbar-header">
+      <span>MENU</span>
+    </header>
+    <section className="navbar-content">
+    </section>
   </div>
 );
 
