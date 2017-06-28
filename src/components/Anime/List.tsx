cc1c4f83e6b7748977804827d3fa1fae898dcf7b
Adhy <adhywiranatap@gmail.com>
2017-06-28 10:47:23 +0700

    add PropTypes to Anime List

--- src/components/Anime/List.tsx
+++ src/components/Anime/List.tsx
@@ -1,28 +1,26 @@
 import * as React from 'react';
 
-import { Anime } from '../../types';
+import { Animes } from '../../types';
 
 interface PropTypes {
-  animes: [Anime];
+  animes: Animes;
 }
 
 export default ({ animes }: PropTypes) => (
   <section className="anime-list">
-    {animes.map((anime, index) => (
-      <div className="anime-card">
+    {animes.map((anime) => (
+      <div key={anime.id} className="anime-card">
         <div className="badge">TRENDING</div>
         <div style={{ flex: 1 }}>
           <img src="https://myanimelist.cdn-dena.com//images//anime//12//85221.jpg" width="100%" />
         </div>
         <div style={{ flex: 1, paddingBottom: 10 }}>
-          <h2>{ anime.title }</h2>
-          <span className="score">{ anime.score[0] }</span>
+          <h2>{anime.title}</h2>
+          <span className="score">{anime.score[0]}</span>
           <div>
             <div className="tag">Shounen</div>
             <div className="tag">Action</div>
             <div className="tag">Seinen</div>
-            {index % 2 === 0 && <div className="tag">Drama</div>}
-            {index % 2 === 0 && <div className="tag">Superhero</div>}
           </div>
         </div>
       </div>
