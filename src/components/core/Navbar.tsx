4867d071a735d002e7ad98fb06222b39b3e4ca9e
Adhy <adhywiranatap@gmail.com>
2017-06-28 13:51:15 +0700

    update navbar styles

--- src/components/core/Navbar.tsx
+++ src/components/core/Navbar.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import './navbar.css';
 
 const Navbar = ({ isNavbarActive, toggleNavbar }: { isNavbarActive: boolean; toggleNavbar: any; }) => (
-  <div className="navbar" style={{ left: isNavbarActive ? 0 : '-71vw' }}>
+  <div className="navbar" style={{ left: isNavbarActive ? 0 : '-81vw' }}>
     wowowoowow
     <button onClick={toggleNavbar}>X</button>
   </div>
