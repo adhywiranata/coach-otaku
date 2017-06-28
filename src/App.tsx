e752eb37a5fd3a251cb668e3866569eea2888328
Adhy <adhywiranatap@gmail.com>
2017-06-28 13:25:08 +0700

    modify constants css and css modules

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import store from './configureStore';
 import './App.css';
 import Header from './components/Header';
 import HomePage from './containers/HomePage';
+import Navbar from './components/core/Navbar';
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <Provider store={store}>
         <div className="app">
+          <Navbar />
           <Header />
           <HomePage />
         </div>
---
+++ src/components/core/Navbar.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './navbar.css';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        wowowoowow
+      </div>
+    );
+  }
+}
+
+export default Navbar;
