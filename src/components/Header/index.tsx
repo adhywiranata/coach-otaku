4f2de2a784f9cce8680518d359e9362203f6082a
Adhy <adhywiranatap@gmail.com>
2017-06-30 09:12:20 +0700

    update Header link to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const menuButton = require('./menu.png');
 const searchButton = require('./search.png');
@@ -6,10 +7,12 @@ const searchButton = require('./search.png');
 export default ({ toggleNavbar, toggleSearch }: { toggleNavbar: any; toggleSearch: any; }) => (
   <div className="app-header">
     <img src={menuButton} width="25px" height="25px" onClick={toggleNavbar} />
-    <h1 className="app-logo">
-      <span className="first">Couch</span>
-      <span className="second">Otaku</span>
-    </h1>
+    <Link to="/" style={{ textDecoration: 'none' }}>
+      <h1 className="app-logo">
+        <span className="first">Couch</span>
+        <span className="second">Otaku</span>
+      </h1>
+    </Link>
     <img src={searchButton} width="25px" height="25px" onClick={toggleSearch} />
   </div>
 );
